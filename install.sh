2e614a5e83ef0d6577670b4e8ca9e3dabe669a6d
kwu <kwu@riotgames.com>
2018-10-07 23:30:16 -0700

    Fixes up problems with bashrc

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2068,SC1090
+
+set -o errexit
+set -o pipefail
+set -o nounset
 
 export ANSIBLE_LOG_PATH=./ansible.log
 
+echo "Bootstrapping local environment..."
+
 ansible-playbook $@ -Ki hosts bootstrap.yml
+
+source "$HOME/.bashrc"
+
+echo "Installing common tooling..."
+
+ansible-playbook $@ -i hosts tools.yml
